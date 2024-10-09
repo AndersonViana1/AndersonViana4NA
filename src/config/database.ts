c61a0d4bec83a9999ac79996508f2a2c7b657778
Anderson Viana <161397203+AndersonViana1@users.noreply.github.com>
2024-10-09 23:24:03 +0000

    feat: Adiciona Helpers e Camadas Auxiliares no Padrão MVC

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,14 @@
-
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 // Substitua pela sua string de conexão do Render.com
-const connectionString = 'postgresql://anderson4na_user:mrTiWXYRJfgd3OEeajqzQNYac9gis2lt@dpg-crh2ont6l47c73c1sb70-a.oregon-postgres.render.com/anderson4na';
+const connectionString =
+  "postgresql://backend_ugaz_user:87KlrSzD5GQ0M8KBs5SaJ0bJgGj5112g@dpg-cs2rppbv2p9s738nq950-a.oregon-postgres.render.com/backend_ugaz";
 
 const pool = new Pool({
   connectionString,
   ssl: {
     rejectUnauthorized: false, // Permite conexões SSL não autorizadas
-  }
+  },
 });
 
 export default pool;
